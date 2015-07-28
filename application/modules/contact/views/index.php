984acf3d42ef232ac807e8a66dc38842b639160b
Marcelo Diniz <marcelo.leo27@gmail.com>
2015-07-28 12:39:12 -0300

    clear page contact

--- application/modules/contact/views/index.php
+++ application/modules/contact/views/index.php
@@ -27,10 +27,6 @@
           <!-- Map Column -->
           <div class="col-md-8">
               <!-- Embedded Google Map -->
-
-              <!-- <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-              src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"
-              ></iframe> -->
               <?php echo $map['html']; ?>
           </div>
           <!-- Contact Details Column -->
