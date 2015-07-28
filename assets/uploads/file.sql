319244c253e2b1b26d4e20b532eb223bf01a136c
Marcelo Diniz <marcelo.leo27@gmail.com>
2015-07-28 00:04:35 -0300

    modify session to use in database and create table ci_sessions

--- assets/uploads/file.sql
+++ assets/uploads/file.sql
@@ -93,3 +93,13 @@ CREATE TABLE `login_attempts` (
   `time` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+        `id` varchar(40) NOT NULL,
+        `ip_address` varchar(45) NOT NULL,
+        `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+        `data` blob NOT NULL,
+        PRIMARY KEY (id),
+        KEY `ci_sessions_timestamp` (`timestamp`)
+);
