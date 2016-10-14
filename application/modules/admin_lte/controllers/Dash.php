91e121f80e93271f9aec49a5a8cda84fb1cd0691
Marcelo Diniz <marcelo.leo27@gmail.com>
2016-10-13 23:05:29 -0300

    finish example admin_lte

--- application/modules/admin_lte/controllers/Dash.php
+++ application/modules/admin_lte/controllers/Dash.php
@@ -22,7 +22,7 @@ class Dash extends Admin_Lte_Controller {
         // Morris chart
         $this->template->add_css('bower_components/morris.js/morris.css');
         // jvectormap
-        $this->template->add_css('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.css');
+        $this->template->add_css('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css');
         // Date Picker
         $this->template->add_css('bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css');
         // Daterange picker
@@ -31,7 +31,7 @@ class Dash extends Admin_Lte_Controller {
         $this->template->add_css('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css');
 
         // jQuery UI 1.11.4
-        // $this->template->add_js('bower_components/https://code.jquery.com/ui/1.11.4/jquery-ui.min.js');
+        $this->template->add_js('bower_components/jquery-ui/jquery-ui.min.js');
         // Resolve conflict in jQuery UI tooltip with Bootstrap tooltip
         // <script>
         //   $.widget.bridge('uibutton', $.ui.button);
@@ -40,21 +40,21 @@ class Dash extends Admin_Lte_Controller {
         $this->template->add_js('bower_components/raphael/raphael.min.js');
         $this->template->add_js('bower_components/morris.js/morris.min.js');
         // Sparkline
-        // $this->template->add_js('bower_components/sparkline/jquery.sparkline.min.js');
+        $this->template->add_js('assets/plugins/sparkline/jquery.sparkline.min.js');
         // jvectormap
         $this->template->add_js('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js');
         $this->template->add_js('assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js');
         // jQuery Knob Chart
-        // $this->template->add_js('bower_components/plugins/knob/jquery.knob.js');
+        $this->template->add_js('bower_components/jquery-knob/dist/jquery.knob.min.js');
         // daterangepicker
-        // $this->template->add_js('bower_components/https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js');
+        $this->template->add_js('bower_components/moment/min/moment.min.js');
         $this->template->add_js('bower_components/bootstrap-daterangepicker/daterangepicker.js');
         // datepicker
         $this->template->add_js('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js');
         // Bootstrap WYSIHTML5
         $this->template->add_js('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js');
         // Slimscroll
-        $this->template->add_js('bower_components/jquery.slimscroll/jquery.slimscroll.min.js');
+        $this->template->add_js('bower_components/jquery-slimscroll/jquery.slimscroll.min.js');
 
         $this->template->add_js('admin_lte/dash/v1.js');
 
@@ -66,6 +66,7 @@ class Dash extends Admin_Lte_Controller {
     {
         $this->template->add_css('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css');
 
+        $this->template->add_js('assets/plugins/sparkline/jquery.sparkline.min.js');
         $this->template->add_js('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js');
         $this->template->add_js('assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js');
         $this->template->add_js('bower_components/Chart.js/Chart.js');
